d26b29cf80102be5e7686de04cd34dd910d7c574
Emilia Poruszek <emilia.poruszek@geek-about.com>
2024-02-26 17:54:41 +0100

    OCP - more testing

--- app/src/test/java/com/resi_tech/solidapp/ReportGeneratorTest.kt
+++ app/src/test/java/com/resi_tech/solidapp/ReportGeneratorTest.kt
@@ -3,23 +3,30 @@ package com.resi_tech.solidapp
 import com.resi_tech.solidapp.ocp.correct.ReportGenerator
 import com.resi_tech.solidapp.ocp.correct.ReportStrategy
 import org.junit.Test
+import org.mockito.Mockito
 
 class ReportGeneratorTest {
+  val reportGenerator = ReportGenerator()
+
+  @Test
+  fun `ReportGenerator work fine with any strategy`() {
+    val mock = Mockito.mock(ReportStrategy::class.java)
+    reportGenerator.generate(mock)
+    Mockito.verify(mock, Mockito.times(1)).generate()
+  }
+
   @Test
   fun `PDF generation should work`() {
-    val reportGenerator = ReportGenerator()
     assert(reportGenerator.generate(ReportStrategy.PDFReport))
   }
 
   @Test
   fun `CSV generation should work`() {
-    val reportGenerator = ReportGenerator()
     assert(reportGenerator.generate(ReportStrategy.CSVReport))
   }
 
   @Test
   fun `HTML generation should throw exception`() {
-    val reportGenerator = ReportGenerator()
     try {
       reportGenerator.generate(ReportStrategy.HTMLReport)
     } catch (e: UnsupportedOperationException) {
